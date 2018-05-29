7e24c593e2381dc9ed585b51a1b5bf5c0fe5a0c9
Dmitri Chtchourov <dchtchou@cisco.com>
2018-05-29 02:16:25 +0000

    beacon: devshell loads

--- src/beacon/beacon-msvc/start-dev.sh
+++ src/beacon/beacon-msvc/start-dev.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-npm install
-nodemon server.js
+cd /root/beatrak/src/beacon/beacon-msvc/app
+LOG_LEVEL=debug LOCPICK_HTTP_HOST=locpick-dep-istio.default.svc.cluster.local LOCPICK_HTTP_PORT=50001 LOCPICK_GRPC_PORT=51001 LOCPICK_GRPC_TLS_PORT=52001 nodemon beacon.js
